4443e23b01909a359a1cbd30f531a0c869dc46c5
DenisPepper <thech@yandex.ru>
2024-04-28 13:01:16 +0300

    Добавит компоненты для Client Side Routing

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import {
   Scripts,
   ScrollRestoration,
   Outlet,
+  Link,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import appStylesHref from "./app.css";
@@ -39,10 +40,10 @@ export default function App() {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
           </nav>
